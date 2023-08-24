5b08bf283b7aed0758bc95d90529b763b5266453
W1ndys <w2769731875@qq.com>
2023-08-24 21:52:20 +0800

    Site updated: 2023-08-24 21:52:20

---
+++ js/biao_qian_e_gao.js
@@ -0,0 +1,16 @@
+// 浏览器搞笑标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function() {
+	if (document.hidden) {
+		$('[rel="icon"]').attr('href', "/funny.ico");
+		document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+		clearTimeout(titleTime);
+	} else {
+		$('[rel="icon"]').attr('href', "/img/newtubiao.png");
+		document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
+		titleTime = setTimeout(function() {
+			document.title = OriginTitle;
+		}, 2000);
+	}
+});
