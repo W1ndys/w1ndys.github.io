6601d7cdf725000d9cf0031fe3f86c9ef08f1906
W1ndys <w2769731875@qq.com>
2023-08-25 09:08:17 +0800

    Site updated: 2023-08-25 09:08:17

--- js/biao_qian_e_gao.js
+++ js/biao_qian_e_gao.js
@@ -3,11 +3,11 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function() {
 	if (document.hidden) {
-		$('[rel="icon"]').attr('href', "/funny.ico");
+		$('[rel="icon"]').attr('href', "/img/favicon.png");
 		document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
 		clearTimeout(titleTime);
 	} else {
-		$('[rel="icon"]').attr('href', "/img/newtubiao.png");
+		$('[rel="icon"]').attr('href', "/img/favicon.png");
 		document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
 		titleTime = setTimeout(function() {
 			document.title = OriginTitle;
