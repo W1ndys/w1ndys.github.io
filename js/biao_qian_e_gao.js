419a89daab4e269ebceb3f9bb998d4a4df79c332
W1ndys <w2769731875@qq.com>
2023-12-22 00:15:23 +0800

    Site updated: 2023-12-22 00:15:22

--- js/biao_qian_e_gao.js
+++ js/biao_qian_e_gao.js
@@ -1,15 +1,15 @@
 // 浏览器搞笑标题
 var OriginTitle = document.title;
 var titleTime;
-document.addEventListener('visibilitychange', function() {
+document.addEventListener('visibilitychange', function () {
 	if (document.hidden) {
 		$('[rel="icon"]').attr('href', "/img/favicon.png");
-		document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+		document.title = '必承其重';
 		clearTimeout(titleTime);
 	} else {
 		$('[rel="icon"]').attr('href', "/img/favicon.png");
-		document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
-		titleTime = setTimeout(function() {
+		document.title = '欲戴皇冠' + OriginTitle;
+		titleTime = setTimeout(function () {
 			document.title = OriginTitle;
 		}, 2000);
 	}
